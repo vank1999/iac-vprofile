c6e319b18e68d67f02816961cd64b033c0156e0e
anil <anilkumarvallepu1@gmail.com>
2024-07-31 14:20:45 +0530

    Bucket name & cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "ak-vprofile-action"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
